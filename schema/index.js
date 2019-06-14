a2c7d2881d613039e03b64500f33da2a99594020
[Derek] <derek@derekgc.com>
2019-06-14 15:11:42 -0400

    update npm packages

--- schema/index.js
+++ schema/index.js
@@ -1,27 +1,27 @@
-// Import type helpers from graphql-js
-const {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString
-} = require('graphql');
+// // Import type helpers from graphql-js
+// const {
+//   GraphQLSchema,
+//   GraphQLObjectType,
+//   GraphQLString
+// } = require('graphql');
 
-// The root query type is where in the data graph
-// we can start asking questions
-const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType',
+// // The root query type is where in the data graph
+// // we can start asking questions
+// const RootQueryType = new GraphQLObjectType({
+//   name: 'RootQueryType',
 
-  fields: {
-    hello: {
-      type: GraphQLString,
-      description: 'The *mandatory* hello world example, GraphQL style',
-      resolve: () => 'world'
-    }
-  }
-});
+//   fields: {
+//     hello: {
+//       type: GraphQLString,
+//       description: 'The *mandatory* hello world example, GraphQL style',
+//       resolve: () => 'world'
+//     }
+//   }
+// });
 
-const ncSchema = new GraphQLSchema({
-  query: RootQueryType
-  // mutation: ...
-});
+// const ncSchema = new GraphQLSchema({
+//   query: RootQueryType
+//   // mutation: ...
+// });
 
-module.exports = ncSchema;
+// module.exports = ncSchema;
